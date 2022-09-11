7943721aad7bfd76b3858d42c25aece06c175544
陈同学 <1575197604@qq.com>
2022-09-11 18:40:29 +0800

    feature:购物车接口

--- order_web/api/shop_cart/shop_cart.go
+++ order_web/api/shop_cart/shop_cart.go
@@ -5,11 +5,17 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 	"net/http"
+	"order_web/forms"
 	"order_web/global"
 	"order_web/proto"
 	"order_web/utils"
+	"strconv"
 )
 
+// List
+// @Description: 获取购物车内商品列表
+// @param ctx
+//
 func List(ctx *gin.Context) {
 	// 获取购物车商品
 	userId, _ := ctx.Get("userId")
@@ -60,14 +66,124 @@ func List(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, reMap)
 }
 
+// New
+// @Description: 添加商品到购物车
+// @param ctx
+//
 func New(ctx *gin.Context) {
 	// 创建购物车表单
+	itemForm := forms.ShopCartItemForm{}
+	err := ctx.ShouldBind(&itemForm)
+	if err != nil {
+		zap.S().Errorw("Error", "message", "购物车表单验证失败", "err", err.Error())
+		utils.HandleValidatorError(ctx, err)
+		return
+	}
+
+	// 添加之前检查商品是否存在
+	_, err = global.GoodsClient.GetGoodsDetail(context.Background(), &proto.GoodsInfoRequest{
+		Id: itemForm.GoodsId,
+	})
+	if err != nil {
+		zap.S().Errorw("Error")
+		utils.HandleGrpcErrorToHttpError(err, ctx)
+		return
+	}
+
+	// 如果添加到购物车数量和商品库存不一致
+	inventoryResp, err := global.InventoryClient.InvDetail(context.Background(), &proto.GoodsInvInfo{
+		GoodsId: itemForm.GoodsId,
+	})
+	if err != nil {
+		zap.S().Errorw("Error", "message", "商品库存查询失败", "err", err.Error())
+		utils.HandleGrpcErrorToHttpError(err, ctx)
+		return
+	}
+
+	if inventoryResp.Num < itemForm.Nums {
+		ctx.JSON(http.StatusOK, gin.H{
+			"nums": "商品库存不足",
+		})
+	}
+
+	userId, _ := ctx.Get("userId")
+	orderResp, err := global.OrderClient.CreateCartItem(context.Background(), &proto.CartItemRequest{
+		GoodsId: itemForm.GoodsId,
+		UserId:  int32(userId.(uint)),
+	})
+	if err != nil {
+		zap.S().Errorw("Error", "message", "添加到购物车失败", "err", err.Error())
+		utils.HandleGrpcErrorToHttpError(err, ctx)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"id": orderResp.Id,
+	})
 }
 
+// Update
+// @Description: 更新购物车商品信息
+// @param ctx
+//
 func Update(ctx *gin.Context) {
+	id := ctx.Param("id")
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		zap.S().Errorw("Error", "message", "param参数id转换失败", "err", err.Error())
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"message": "url格式错误",
+		})
+		return
+	}
 
+	itemForm := forms.ShopCartItemUpdateForm{}
+	err = ctx.ShouldBind(&itemForm)
+	if err != nil {
+		zap.S().Errorw("Error", "message", "updateForm表单验证失败", "err", err.Error())
+		utils.HandleValidatorError(ctx, err)
+		return
+	}
+
+	userId, _ := ctx.Get("userId")
+	_, err = global.OrderClient.UpdateCartItem(context.Background(), &proto.CartItemRequest{
+
+		UserId:  int32(userId.(uint)),
+		GoodsId: int32(idInt),
+		Nums:    itemForm.Nums,
+		Checked: *itemForm.Checked,
+	})
+	if err != nil {
+		zap.S().Errorw("Error", "message", "更新", "err", err.Error())
+		utils.HandleGrpcErrorToHttpError(err, ctx)
+		return
+	}
 }
 
+// Delete
+// @Description: 删除购物车内商品
+// @param ctx
+//
 func Delete(ctx *gin.Context) {
+	id := ctx.Param("id")
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		zap.S().Errorw("Error", "message", "param参数id转换失败", "err", err.Error())
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"message": "url格式错误",
+		})
+		return
+	}
 
+	userId, _ := ctx.Get("userId")
+	_, err = global.OrderClient.DeleteCartItem(context.Background(), &proto.CartItemRequest{
+		UserId:  int32(userId.(uint)),
+		GoodsId: int32(idInt),
+	})
+	if err != nil {
+		zap.S().Errorw("Error", "message", "删除购物车记录失败", "err", err.Error())
+		utils.HandleGrpcErrorToHttpError(err, ctx)
+		return
+	}
+	ctx.Status(http.StatusOK)
 }
--- order_web/router/shop_cart.go
+++ order_web/router/shop_cart.go
@@ -9,9 +9,9 @@ import (
 func InitShopCartRouter(Router *gin.RouterGroup) {
 	ShopCartRouter := Router.Group("shopcarts")
 	{
-		ShopCartRouter.GET("", middlewares.JWTAuth(), shop_cart.List) //购物车列表
-		ShopCartRouter.DELETE("/:id", shop_cart.Delete)               //删除条目
-		ShopCartRouter.POST("", shop_cart.New)                        //添加商品到购物车
-		ShopCartRouter.PATCH("/:id", shop_cart.Update)                //修改条目
+		ShopCartRouter.GET("", middlewares.JWTAuth(), shop_cart.List)          //购物车列表
+		ShopCartRouter.DELETE("/:id", middlewares.JWTAuth(), shop_cart.Delete) //删除条目
+		ShopCartRouter.POST("", middlewares.JWTAuth(), shop_cart.New)          //添加商品到购物车
+		ShopCartRouter.PATCH("/:id", middlewares.JWTAuth(), shop_cart.Update)  //修改条目
 	}
 }
